5872d10da1a2735b26719c67bc53a0a07a51d4b5
Daniel Campos <dacamp@microsoft.com>
2019-02-21 14:45:52 -0800

    Updates To Conversational Search

--- ConversationalSearch/generateArtificialSessions.py
+++ ConversationalSearch/generateArtificialSessions.py
@@ -5,7 +5,6 @@ import sys
 from annoy import AnnoyIndex
 
 TREESIZE = 10000
-EMBEDDINGDIM = 100
 
 def loadQueries(filename):
     queries = set()
@@ -13,17 +12,19 @@ def loadQueries(filename):
         for l in f:
             queries.add(l.strip())
     return queries
+
 def loadSessions(filename):
     sessions = []
     with open(filename,'r'):
         for l in f:
             sessions.append(l.strip().split('\t'))
     return sessions
+
 def loadVectors(filename, realQueries):
     i = 0
     j = 0
-    artificial = [{},{},[]]
-    real = [{},{},[]]
+    artificial = [{},{},[]] #Query2Idx, idX2Query, id2Vector
+    real = [{},{},[]] #Query2Idx, idX2Query, id2Vector
     with open(filename,'r') as f:
         for l in f:
             l = l.strip().split('\t')
@@ -40,9 +41,10 @@ def loadVectors(filename, realQueries):
                 real[2].append(np.array(vectors,dtype=float))
                 j += 1
     return real, artificial
-def generateAnnoy(real, artificial, annoyFilename):
+
+def generateAnnoy(real, artificial, annoyFilename, dimensions):
     idx2vec = np.array(artificial[2])
-    t = AnnoyIndex(EMBEDDINGDIM)
+    t = AnnoyIndex(dimensions)
     for j in range(len(artificial[2])):
         t.add_item(j,idx2vec[j])
     print('Done Adding items to AnnoyIndex')
@@ -50,12 +52,43 @@ def generateAnnoy(real, artificial, annoyFilename):
     print('Done Building AnnoyIndex')
     t.save(annoyFilename)
     return t
+
+def generateArtificialSessions(queryVectors, sessions, annoyEmbedding, filename):
+    with open(filename,'w') as w:
+        for session in sessions:
+            queriesUsed = set()
+            output = ''
+            for query in sessions[session]:
+                vectorIndex = queryVectors[0][query]
+                v = queryVectors[2][vectorIndex]
+                artificialQueries = annoyEmbedding.get_nns_by_vector(v, 10, search_k=-1, include_distances=False)
+                for artificialQuery in artificialQueries:
+                    if artificialQuery not in queriesUsed: #ensure session isnt just repeating queries
+                        queriesUsed.add(artificialQuery)
+                        output += '{}\t'.format(artificialQuery)
+                        break
+            w.write("{}\n".format(output[:-1]))
+    
 if __name__ == "__main__":
-    if len(sys.argv) != 7:
-        print("Usage: makeArtificialSessions.py <realQueries> <queryVectors> <annFilename> <sessions> <outputFolder>")
+    if len(sys.argv) != 6:
+        print("Usage: makeArtificialSessions.py <realQueries> <queryVectors> <BERTVectors> <annFilename> <sessions>")
         exit(-1)
     else:
+        print("Loading Queries and Sessions")
         realQueries = loadQueries(sys.argv[1])
+        print("Loading Sessions")
+        sessions = loadSessions(sys.argv[5])
+        #Run regular embeddings
+        print("Loading Query Vectors")
         real, artificial = loadVectors(sys.argv[2], realQueries)
-        t = generateAnnoy(real, artificial, sys.argv[3])
-        sessions = loadSessions(sys.argv[])
+        print("Building Annnoy Query Embeddings")
+        annoyEmbedding = generateAnnoy(real, artificial, sys.argv[4], 100)
+        print("Generating Sessions Query Embeddings")
+        generateArtificialSessions(queryVectors, sessions, annoyEmbedding, filename)
+        #Run on BERT embeddings
+        print("Loading BERT Vectors")
+        real, artificial = loadVectors(sys.argv[3], realQueries)
+        print("Building Annnoy Query Embeddings")
+        annoyEmbedding = generateAnnoy(real, artificial, 'BERT' + sys.argv[4], 1024)
+        print("Generating Sessions Query Embeddings")
+        generateArtificialSessions(queryVectors, sessions, annoyEmbedding, filename)
--- ConversationalSearch/generateQuerysets.py
+++ ConversationalSearch/generateQuerysets.py
@@ -1,9 +1,6 @@
 import json
-<<<<<<< HEAD
 import sys
 import os
-=======
->>>>>>> 544545b189773dd586c6c2aaa560f686432321aa
 def loadNQ(dirPath):
     files = os.listdir(dirPath)
     queries = set()
@@ -21,14 +18,9 @@ def loadQuora(filename):
     with open(filename) as f:
         for l in f:
             l = l.strip().split('\t')
-<<<<<<< HEAD
             if len(l) > 4:
                 queries.add(l[3])
                 queries.add(l[4])
-=======
-            queries.add(l[3])
-            queries.add(l[4])
->>>>>>> 544545b189773dd586c6c2aaa560f686432321aa
     return queries
 def loadMSMARCO(files):
     #1048578 cost of endless pools/swim spa
@@ -38,7 +30,6 @@ def loadMSMARCO(files):
             for l in f:
                 queries.add(l.strip().split('\t')[1])
     return queries       
-<<<<<<< HEAD
 def loadUnusedMSMARCO(filename):
     #AnswerIsInPassage\tJudgeID\tHitGroupDataInt\tHitDataInt\tHitState\tJudgmentState\tJudgmentDataInt\tJudgmentDataIntName\tJudgmentSubmitTime\tJudgmentTypeID\tTimeSpentOnJudgment\tHitGroupID\tHitID\tQueryId\tQueryText\tPassagesJson\tJudgmentID\tjudgment\tanswerTextArea\tnopassageSubmit\texplainNoAnswer\texplainCantJudge\tSL_locer\tSL_lng_from\tSL_lng_to\tJudgmentType\tGoldHitType\tGoldHitComments\t@RealTimeAuditComment\tConsensus\n'
     queries = set
@@ -94,35 +85,11 @@ if __name__ == "__main__":
         print("Done reading MSMARCO")
         quoraQueries = loadQuora(sys.argv[4])
         print("Done reading quora")
-        nqQueries = loadNQ(sys.argv[5])
+        #nqQueries = loadNQ(sys.argv[5])
+        nqQueries = [] # NQ TOS do not allow us to use the dataset for this type of research
         print("Done reading NQ")
-        unusedMSMARCO = loadUnusedMSMARCO(sys.argv[6])
+        #unusedMSMARCO = loadUnusedMSMARCO(sys.argv[6])
+        unusedMSMARCO = []
         print("Done reading Unused")
         sessions, realQueries = loadSessions(sys.argv[7])
         writeData(msmarcoQueries, quoraQueries, nqQueries, unusedMSMARCO, realQueries, sessions)
-=======
-def writeData(msmarcoQueries, quoraQueries, nqQueries, dirPath)
-    allQueries = msmarcoQueries + quoraQueries + nqQueries
-    print("There are {} unique MSMARCO Queries, {} unique Quora Duplicate Queries, {} unique NQ Queries for a total of {} unique queries.".format(len(msmarcoQueries), len(quoraQueries), len(nqQueries), len(allQueries)))
-    with open(os.path.join(dirPath, 'msmarcoQueries.tsv'),'w') as w:
-        for query in msmarcoQueries:
-            w.write('{}\n'.format(query))
-    with open(os.path.join(dirPath, 'quoraQueries.tsv'),'w') as w:
-        for query in quoraQueries:
-            w.write('{}\n'.format(query))
-    with open(os.path.join(dirPath, 'nqQueries.tsv'),'w') as w:
-        for query in nqQueries:
-            w.write('{}\n'.format(query))
-    with open(os.path.join(dirPath, 'allQueries.tsv'),'w') as w:
-        for query in allQueries:
-            w.write('{}\n'.format(query))
-if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        print("Usage: generateQuerysets.py <msmarco train queries> <msmarco dev queries> <msmarco eval queries> <quoraQueries> <NQFolder> <outputFolder>")
-        exit(-1)
-    else:
-        msmarcoQueries = loadMSMARCO([sys.argv[1],sys.argv[2],sys.argv[3]])
-        quoraQueries = loadQuora(sys.argv[4])
-        nqQueries = loadNQ(sys.argv[5])
-        writeData(msmarcoQueries, quoraQueries, nqQueries, sys.argv[6])
->>>>>>> 544545b189773dd586c6c2aaa560f686432321aa
